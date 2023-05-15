3ab0714a765b9a0d003e82169171937d8e3c04f1
Mark-andrew7 <kamaumark19@gmail.com>
2023-05-14 23:30:29 -0700

    BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
         """
         initializer
         """
-        if kwargs is not None:
+        if kwargs:
             for k, v in kwargs.items():
                 if k == "created_at":
                     v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
@@ -20,11 +20,11 @@ class BaseModel:
                     v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                 if k != "__class__":
                     setattr(self, k, v)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = datetime.now()
-                    models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
