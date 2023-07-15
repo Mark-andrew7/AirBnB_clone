bed606c3d017ec7ae3fe503db4eb5da6febc25c4
Fredrick-Kiboi <fredrickiboi@gmail.com>
2023-07-15 18:34:52 +0300

    test_update :test_tube:

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -72,3 +72,38 @@ class Test_BaseModel(unittest.TestCase):
         """Test if the '__class__' is present and correct"""
         self.assertTrue(model_dict.get('__class__'))
         self.assertEqual(model_dict['__class__'], 'BaseModel')
+
+    def test_kwargs_attribute_assignment(self):
+        """Testing attribute assignment with kwargs.
+        I do this by creating a dictionary with some values and pass
+        it to the 'BaseMode' initializer function. Then i check if these
+        values are correctly assigned as attributes to 'BaseModel' instance
+        """
+        kwargs = {"id": "123", "created_at": "2023-07-13T12:59:36.620743",
+                  "updated_at": "2023-07-13T12:59:36.620743"}
+        model = BaseModel(**kwargs)
+
+        self.assertEqual(model.id, "123")
+        self.assertEqual(model.created_at,
+                         datetime.datetime(2023, 7, 13, 12, 59, 36, 620743))
+        self.assertEqual(model.updated_at,
+                         datetime.datetime(2023, 7, 13, 12, 59, 36, 620743))
+
+    def test_class_not_attribute(self):
+        """Testing that '__class__' is not assigned as attribute"""
+        kwargs = {"__class__": "SomeClass"}
+        model = BaseModel(**kwargs)
+
+        self.assertNotIn("__class__", model.__dict__)
+
+    def test_automatic_id_created_at(self):
+        """Testing automatic 'id' and 'created_at' on new instance
+        if no 'kwargs' are provided the 'BaseModel' should automatically assing
+        an 'id' and 'created_at' attributes
+        """
+        model = BaseModel()
+
+        self.assertTrue(hasattr(model, "id"))
+        self.assertTrue(isinstance(model.id, str))
+        self.assertTrue(hasattr(model, "created_at"))
+        self.assertTrue(isinstance(model.created_at, datetime.datetime))
